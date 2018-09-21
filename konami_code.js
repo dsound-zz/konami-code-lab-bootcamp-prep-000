f7e8402103465a509d71fbae6b50e2b83451369c
demian sims <demiansims@gmail.com>
2018-09-21 20:36:02 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,24 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  let index = 0;
+  
+  function onKeyDownHandler(e) {
+    const key = e.key;
+  
+  if (key === codes[index]) {
+    index++;
+    
+    if (index === alphabet.length) {
+      alert("Hurray!"); 
+      
+      index = 0; 
+    }
+  } else {
+    index = 0;
+  }
+        
+      }
 }
+  
+
